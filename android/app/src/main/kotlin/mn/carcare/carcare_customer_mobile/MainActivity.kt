b5c73ceeade9f6cd78e3eabd7f53af753f07dcee
ochroo0705 <ochroo0705@gmail.com>
2026-09-01 18:55:59 +0800

    android maps false negative fix

--- android/app/src/main/kotlin/mn/carcare/carcare_customer_mobile/MainActivity.kt
+++ android/app/src/main/kotlin/mn/carcare/carcare_customer_mobile/MainActivity.kt
@@ -3,6 +3,7 @@ package mn.carcare.carcare_customer_mobile
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import android.content.pm.PackageManager
 
 class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
@@ -15,7 +16,11 @@ class MainActivity : FlutterActivity() {
                 result.notImplemented()
                 return@setMethodCallHandler
             }
-            val value = applicationInfo.metaData
+            @Suppress("DEPRECATION")
+            val value = packageManager.getApplicationInfo(
+                packageName,
+                PackageManager.GET_META_DATA,
+            ).metaData
                 ?.getString("com.google.android.geo.API_KEY")
                 ?.trim()
             result.success(!value.isNullOrEmpty() && !value.contains("${'$'}{"))
